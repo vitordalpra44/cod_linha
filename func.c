59ba33cbfeb49f395b2c8dbe58039d39a8fefea6
Vitor Dalpra <vitordalpra@alunos.utfpr.edu.br>
2023-06-02 12:21:25 -0300

    att

---
+++ func.c
@@ -0,0 +1,78 @@
+#include "func.h"
+
+
+void encrypt(unsigned char* msg, int key){
+    int msg_size = strlen(msg);
+    for(int i = 0;i<msg_size;i++) msg[i]=(msg[i]+key);
+}
+
+void decrypt(unsigned char* msg, int key){
+    int msg_size = strlen(msg);
+    for(int i =0;i<msg_size;i++) msg[i] = (msg[i]-key);
+}
+
+void printBin(unsigned char* msg, int msg_size) {
+    printf("\t");
+    while (msg_size) {
+        unsigned char *msg_buf;
+        unsigned char c = *msg++;
+        for (int i = 7; i >= 0; i--) {
+            printf("\t%d", (c >> i) & 1);
+        }
+        msg_size--;
+    }
+    printf("\n");
+}
+
+void amiCode(unsigned char* msg, char* ami_msg, int msg_size) {
+    int lastBit = 1; // Inicialmente, o último bit é 1
+    int i, j, cont=0;
+    for (i = 0; i < msg_size ; i++) {
+        unsigned char character = msg[i];
+        for (j = 7; j >= 0; j--) {
+            int bit = (character >> j) & 1;
+            if (bit == 0) {
+                ami_msg[cont]= 0;
+                cont++;
+            } else {
+                if (lastBit == -1) {
+                    ami_msg[cont]= 1;
+                    lastBit = 1;
+                } else {
+                    ami_msg[cont]= -1;
+                    lastBit = -1;
+                }
+                cont++;
+            }
+        }
+    }
+    ami_msg[cont] = '@'; //condicão de final da string;
+}
+
+void amiDecode(char* ami_msg, int msg_size, unsigned char* msg) {
+    int i, j;
+    int binary[8];
+    
+    // Decodificar valores AMI para binário
+    for (i = 0; i < 8; i++) {
+        binary[i] = ami_msg[i];
+        if (binary[i] == -1)
+            binary[i] = 1;
+    }
+    
+    // Converter binário para decimal
+    unsigned char asciiValue = 0;
+    for (i = 0; i < 8; i++) {
+        asciiValue <<= 1; // Deslocar bits para a esquerda
+        asciiValue |= binary[i];
+    }
+    
+    return asciiValue;
+}
+
+void printAMI(char* msg) {
+    for (int i = 0; msg[i]!= '@'; i++) {
+        printf("\t%d", msg[i]);
+    }
+    printf("\n");
+}
